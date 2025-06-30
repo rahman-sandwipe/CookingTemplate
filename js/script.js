95bd495baaf9ab11a74bc355dd647dd6000b2077
tarikbd <tarikzaman.bd@gmail.com>
2025-06-30 11:46:28 +0600

    Replaced all files with new set

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+  const toggleBtn = document.getElementById('menu-toggle');
+  const navMenu = document.getElementById('nav-menu');
+
+  toggleBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+  });
+
